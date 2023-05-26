9c18856e52e11b33b6eee808b89b6d184fecbd04
faststeve22 <faststeve22@gmail.com>
2023-05-26 14:56:43 -0400

    Added CRUD methods to EquipmentDAO.

--- DataAccessLayer/DAO/EquipmentDAO.cs
+++ DataAccessLayer/DAO/EquipmentDAO.cs
@@ -1,4 +1,7 @@
 ﻿using Logbook.DataAccessLayer.Interfaces;
+using Logbook.Models;
+using Microsoft.AspNetCore.Antiforgery;
+using System.Data;
 
 namespace Logbook.DataAccessLayer.DAO
 {
@@ -8,6 +11,78 @@ namespace Logbook.DataAccessLayer.DAO
         public EquipmentDAO(IDbConnectionFactory connectionFactory)
         {
             _connectionFactory = connectionFactory;
+        }
+
+        public void AddEquipment(Equipment equipment)
+        {
+            using(IDbConnection conn = _connectionFactory.CreateConnection())
+            {
+                conn.Open();
+                IDbCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO Equipment (equipment_brand, equipment_model, equipment_type) VALUES (@equipmentBrand, @equipmentModel, @equipmentType)";
+                AddParameter(cmd, "@equipmentBrand", equipment.EquipmentBrand);
+                AddParameter(cmd, "@equipmentModel", equipment.EquipmentModel);
+                AddParameter(cmd, "@equipmentType", equipment.EquipmentType);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public Equipment GetEquipment(int equipmentId)
+        {
+            using (IDbConnection conn = _connectionFactory.CreateConnection())
+            {
+                conn.Open();
+                IDbCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT equipment_id, equipment_brand, equipment_model, equipment_type FROM Equipment WHERE equipment_id = @equipmentId";
+                AddParameter(cmd, "@equipmentId", equipmentId);
+                IDataReader reader = cmd.ExecuteReader();
+                return EquipmentReader(reader);
+            }
+        }
+
+        public void UpdateEquipment(Equipment equipment)
+        {
+            using (IDbConnection conn = _connectionFactory.CreateConnection())
+            {
+                conn.Open();
+                IDbCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "UPDATE Equipment SET equipment_brand = @equipmentBrand, equipment_model = @equipmentModel, equipment_type = @equipmentType WHERE equipment_id = @equipmentId";
+                AddParameter(cmd, "@equipmentId", equipment.EquipmentId);
+                AddParameter(cmd, "@equipmentBrand", equipment.EquipmentBrand);
+                AddParameter(cmd, "@equipmentModel", equipment.EquipmentModel);
+                AddParameter(cmd, "@equipmentType", equipment.EquipmentType);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteEquipment(int equipmentId)
+        {
+            using (IDbConnection conn = _connectionFactory.CreateConnection())
+            {
+                conn.Open();
+                IDbCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "DELETE FROM Equipment WHERE equipment_id = @equipmentId";
+                AddParameter(cmd, "@equipmentId", equipmentId);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private Equipment EquipmentReader(IDataReader reader)
+        {
+            Equipment equipment = new Equipment();
+            equipment.EquipmentId = Convert.ToInt32(reader["equipment_id"]);
+            equipment.EquipmentBrand = Convert.ToString(reader["equipment_brand"]);
+            equipment.EquipmentModel = Convert.ToString(reader["equipment_model"]);
+            equipment.EquipmentType = Convert.ToString(reader["equipment_type"]);
+            return equipment;
+        }
+
+        private void AddParameter(IDbCommand cmd, string paramName, object value)
+        {
+            var Parameter = cmd.CreateParameter();
+            Parameter.ParameterName = paramName;
+            Parameter.Value = value;
+            cmd.Parameters.Add(Parameter);
         }
     }
 }
